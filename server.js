058dedc7b2470408d61438d91154fb413a8b2f9f
Dima Zeklam <dema.ziklam@gmail.com>
2021-08-23 18:24:13 +0300

    backend

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const PORT = process.env.PORT;
 const server = express();
 server.use(cors());
+const BookSchema = require('./model');
+const bookModel = require('./model');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/books', {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -15,44 +17,13 @@ function listenerHandl(req, res) {
     res.send('all good');
 }
 
-const BookSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    status: String,
-    email: String
-})
 
-const bookModel = mongoose.model('books',BookSchema);
-
-function seedDataCollection() {
-    const Potter = new bookModel({
-        title: 'Harry Potter and the Goblet of Fire',
-        description: 'A generation grew up on Rowling all-conquering magical fantasies, but countless adults have also been enthralled by her immersive world. Book four, the first of the doorstoppers, marks the point where the series really takes off. The Triwizard Tournament provides pace and tension, and Rowling makes her boy wizard look death in the eye for the first time.',
-        status: 'read',
-        email: 'dema.ziklam@gmail.com'
-    })
-    const LittleLife = new bookModel({
-        title: 'A Little Life',
-        description: 'This operatically harrowing American gay melodrama became an unlikely bestseller, and one of the most divisive novels of the century so far. One man’s life is blighted by abuse and its aftermath, but also illuminated by love and friendship. Some readers wept all night, some condemned it as titillating and exploitative, but no one could deny its power.',
-        status: 'read',
-        email: 'dema.ziklam@gmail.com'
-    })
-    const TheCost = new bookModel({
-        title: 'The Cost of Living',
-        description: 'Chaos is supposed to be what we most fear but I have come to believe it might be what we most want ... ” The second part of Levy’s “living memoir”, in which she leaves her marriage, is a fascinating companion piece to her deep yet playful novels. Feminism, mythology and the daily grind come together for a book that combines emotion and intellect to dazzling effect.',
-        status: 'read',
-        email: 'dema.ziklam@gmail.com'
-    })
-    Potter.save();
-    LittleLife.save();
-    TheCost.save();
-}
 // seedDataCollection();
 
-// localhost:3001/books?emailaddress=
+// localhost:3001/books?email=
 function getbook(req,res) {
     console.log('inside getbook func')
-    let emailaddress2 = req.query.emailaddress;
+    let emailaddress2 = req.query.email;
     bookModel.find({email:emailaddress2},function(err,bookData){
         if(err) {
             console.log('error in getting the data')
